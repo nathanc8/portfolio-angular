ba5880cb0ae6fc4387169bea8ff499eadd5ec878
Nathan Cazard <cazard.nathan@gmail.com>
2024-11-30 01:52:33 +0100

    add user id

--- src/app/env/env.dev.ts
+++ src/app/env/env.dev.ts
@@ -3,5 +3,5 @@ import * as process from 'process';
 export const env = {
     emailServiceID: process.env['NG_APP_EMAIL_SERVICE_ID'],
     emailTemplateID: process.env['NG_APP_EMAIL_TEMPLATE_ID'],
-    emailUserID: process.env['NG_APP_EMAIL__ID'],
+    emailUserID: process.env['NG_APP_EMAIL_USER_ID'],
 };
